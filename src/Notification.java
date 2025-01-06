0738d120bb1ba9a970ce95e410f181786dd5c21e
giorgizena123 <zenaishvili621@gmail.com>
2025-01-06 20:49:47 +0400

    Update Notification.java

--- src/Notification.java
+++ src/Notification.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
 
-public class Notification {
+abstract class Notification {
+    private String sender;
+    private String receiver;
+    private String messageText;
+
+
+    public Notification(String sender, String receiver, String messageText) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.messageText = messageText;
+    }
+
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+
+    public abstract void send();
+    public abstract void receive();
 }
+
