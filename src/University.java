968421f4c9f6a20ef90c5f8ab5d196589773f35e
giorgizena123 <zenaishvili621@gmail.com>
2025-01-06 20:50:08 +0400

    Update University.java

--- src/University.java
+++ src/University.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
-
 public class University {
-}
+    private String name;
+    private boolean isPublicUniversity;
+    private int facultyCount;
+
+
+    public University(String name, boolean isPublicUniversity, int facultyCount) {
+        this.name = name;
+        this.isPublicUniversity = isPublicUniversity;
+        this.facultyCount = facultyCount;
+    }
+
+
+    public class Student {
+        private String firstName;
+        private String lastName;
+        private int age;
+        private String personalId;
+        private double averageGrade;
+        private int year;
+
+
+        public Student(String firstName, String lastName, int age, String personalId, double averageGrade, int year) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.personalId = personalId;
+            this.averageGrade = averageGrade;
+            this.year = year;
+        }
+
+
+        public void printInfo() {
+            System.out.println("Student: " + firstName + " " + lastName);
+            System.out.println("Age: " + age);
+            System.out.println("Personal ID: " + personalId);
+            System.out.println("Average Grade: " + averageGrade);
+            System.out.println("Year: " + year);
+        }
+    }
+
+
+    public void printInfo() {
+        System.out.println("University: " + name);
+        System.out.println("Public University: " + (isPublicUniversity ? "Yes" : "No"));
+        System.out.println("Number of Faculties: " + facultyCount);
+    }
+
+
+    }
