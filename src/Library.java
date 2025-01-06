2073c5cccd66cf734f459c33fe71f753881d6e46
giorgizena123 <zenaishvili621@gmail.com>
2025-01-06 20:49:14 +0400

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class Library {
-}
+    private int id;
+    private String name;
+
+
+    public Library(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public class Book {
+        private int bookId;
+        private String bookName;
+
+
+        public Book(int bookId, String bookName) {
+            this.bookId = bookId;
+            this.bookName = bookName;
+        }
+
+
+        public void printInfo() {
+            System.out.println("Book ID: " + bookId);
+            System.out.println("Book Name: " + bookName);
+        }
+    }
+
+
+    public void printInfo() {
+        System.out.println("Library ID: " + id);
+        System.out.println("Library Name: " + name);
+    }
+
+
+    }
