bce3ebc017b5a76d07f196fb4168984d26298008
giorgizena123 <zenaishvili621@gmail.com>
2025-01-06 20:49:33 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Notification sms = new Notification("John", "Jane", "Happy New Year!") {
+            @Override
+            public void send() {
+                System.out.println("Sending SMS: " + getMessageText());
+            }
+
+            @Override
+            public void receive() {
+                System.out.println("Receiving SMS: " + getMessageText());
+            }
+        };
+
+        sms.send();
+        sms.receive();
+
+
+        Computer computer = new Computer("USA", "Dell", 1200.0, true);
+
+
+        Computer.Monitor monitor = computer.new Monitor(24.0, "1920x1080", 60);
+        Computer.Motherboard motherboard = computer.new Motherboard(4, "DDR4", 2400.0);
+
+        monitor.printInfo();
+        motherboard.printInfo();
+        Library library = new Library(1, "Central Library");
+        library.printInfo();
+
+
+        Library.Book book = library.new Book(101, "Java Programming");
+        book.printInfo();
+        University university = new University("Tech University", true, 6);
+        university.printInfo();
+
+
+        University.Student student = university.new Student("John", "Doe", 20, "ID12345", 8.5, 2);
+        student.printInfo();
+        Car[] cars = new Car[2];
+        cars[0] = new Car("Toyota", "Corolla", "XYZ123");
+        cars[1] = new Car("BMW", "X5", "ABC456");
+
+
+        Car.Engine engine1 = cars[0].new Engine("Gasoline", 4, 500000);
+        Car.Engine engine2 = cars[1].new Engine("Diesel", 6, 700000);
+
+
+        Car.Engine maxMileageEngine = engine1.getMileage() > engine2.getMileage() ? engine1 : engine2;
+        System.out.println("Car with the highest mileage:");
+        maxMileageEngine.printInfo();
     }
 }
+
+
+
